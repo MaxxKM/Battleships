02b6ed7eac699a8ea177a1d2c7760d055b35eae7
MaxxKM <maxkellymahon@gmail.com>
2024-03-12 16:48:19 +0000

    add place_ship function to randomly place ships

--- run.py
+++ run.py
@@ -4,6 +4,24 @@ Creates a 5 x 5 board filled with '.' for empty spaces
 def create_board():
     return [['.' for i in range(5)] for i in range(5)]
 
+"""
+Places ships at random positions
+"""
+def place_ship(board, occupied_positions):
+    while True:
+        row = random.randint(0, 4)
+        col = random.randint(0, 4)
+        position = (row, col)
+        """
+        Check if the position is empty and not already occupied
+        """
+        if board[row][col] == '.' and position not in occupied_positions:
+            board[row][col] = '@'
+            occupied_positions.append(position)
+            return
+
+
+
 def get_player_name():
     """
     Gets the name of the player from their input
