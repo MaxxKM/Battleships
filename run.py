0f65fbd28c626a2c1107ea8f09060167b8dc4fcb
MaxxKM <maxkellymahon@gmail.com>
2024-03-12 16:21:42 +0000

    add create_board function to create the 5 by 5 board

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
+"""
+Creates a 5 x 5 board filled with '.' for empty spaces
+"""
+def create_board():
+    return [['.' for i in range(5)] for i in range(5)]
+
 def get_player_name():
+    """
+    Gets the name of the player from their input
+    """
     return input("Enter you name: ")
 
 def main():
     player_name = get_player_name()
-    print(f"Welcome {player_name}")
+    print(f"Welcome {player_name} to a game of Battleships!")
+    print("The board is 5 x 5 in size")
 
 main()
